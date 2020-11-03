6fd90da1258ef259921929a4e7de0225adcc4162
tracer-ics2016 <tracer@njuics.org>
2020-11-03 13:56:23 +0000

    >  compile NEMU 3019244092 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:56:23 up 3 days, 23:27, 14 users,  load average: 1.24, 1.10, 0.94 756fdf51ec2d25fdf0e279e1ee8baf27d3d2fa8c

--- nemu/src/cpu/exec/logic/sar-template.h
+++ nemu/src/cpu/exec/logic/sar-template.h
@@ -5,17 +5,17 @@
 static void do_execute () {
 	DATA_TYPE src = op_src->val;
 	DATA_TYPE_S dest = op_dest->val;
-	DATA_TYPE_S result = op_dest->val;
+
 	uint8_t count = src & 0x1f;
 	dest >>= count;
-	result = dest;
+	OPERAND_W(op_dest, dest);
 	int len = (DATA_BYTE << 3) - 1;
+	int result = dest;
 	cpu.CF=0;
 	cpu.OF=0;
 	cpu.SF=result >> len;
     	cpu.ZF=!result;
-    	OPERAND_W(op_dest, dest);
-	result ^= result >>4;
+		result ^= result >>4;
 	result ^= result >>2;
 	result ^= result >>1;
 	cpu.PF=!(result & 1);
