ccf8a4f7c127f743f4e38beb60d45c1078388ed0
tracer-ics2016 <tracer@njuics.org>
2020-12-18 09:21:02 +0000

    >  compile NEMU 3019244092 simpleedu Linux simpleedu 4.15.0-128-generic #131-Ubuntu SMP Wed Dec 9 06:57:35 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:21:02 up 33 min,  1 user,  load average: 1.64, 1.39, 1.32 4126d1458dfe44f5379d0fa362938ae1a96c970

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -6,10 +6,6 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <memory.h>
-
-void cpu_exec(uint32_t);
-
 
 typedef struct{
 	swaddr_t prev_ebp;
@@ -18,10 +14,10 @@ typedef struct{
 
 }PSF;
 
-void get_func_from_addr(char *tmp, swaddr_t addr);
 
+void cpu_exec(uint32_t);
 void display_reg();
-
+void get_func_from_addr(char *tmp,swaddr_t addr);
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 char* rl_gets() {
 	static char *line_read = NULL;
@@ -38,6 +34,40 @@ char* rl_gets() {
 	}
 
 	return line_read;
+}
+
+static void read_ebp(swaddr_t addr, PSF *ebp) {
+	ebp->prev_ebp = swaddr_read(addr, 4,R_SS);
+	ebp->ret_addr = swaddr_read(addr+4, 4,R_SS);
+	int i;
+	for ( i = 0; i < 4; i++)
+	{
+		ebp->args[i] = swaddr_read(addr+8+4*i, 4,R_SS);
+	}
+	
+}
+
+static int  cmd_bt(char * args){
+	int a = 0 ;
+	PSF current_ebp;
+	char tmp[32];
+
+	swaddr_t addr = reg_l(R_EBP);
+	current_ebp.ret_addr = cpu.eip;
+	while(addr>0){
+		printf("#%d	0x%08x in\t",a++,current_ebp.ret_addr);
+
+		get_func_from_addr(tmp,current_ebp.ret_addr);
+
+		printf("%s\t",tmp);
+		read_ebp(addr,&current_ebp);
+		if(strcmp(tmp,"main")==0)printf("( )\n");
+		else {
+			printf("( %d, %d, %d, %d )\n", current_ebp.args[0], current_ebp.args[1], current_ebp.args[2], current_ebp.args[3]);
+		}
+		addr = current_ebp.prev_ebp;
+	}
+	return 0;
 }
 
 /* TODO: Add single step */
@@ -132,6 +162,20 @@ static int cmd_d(char *args) {
 	return 0;
 }
 
+static int cmd_page(char *args) {
+	if (args == NULL) return 0;
+	lnaddr_t lnaddr;
+	sscanf(args, "%x", &lnaddr);
+	hwaddr_t hwaddr = page_translate(lnaddr, 1);
+	if (!cpu.cr0.protect_enable || !cpu.cr0.paging)
+	{
+		printf("\033[1;33mPage address convertion is invalid.\n\033[0m");
+	}
+	printf("0x%x -> 0x%x\n", lnaddr, hwaddr);
+	return 0;
+	
+}
+
 static int cmd_c(char *args) {
 	cpu_exec(-1);
 	return 0;
@@ -140,42 +184,6 @@ static int cmd_c(char *args) {
 static int cmd_q(char *args) {
 	return -1;
 }
-
-static void read_ebp(swaddr_t addr, PSF *ebp) {
-	ebp->prev_ebp = swaddr_read(addr, 4,R_SS);
-	ebp->ret_addr = swaddr_read(addr+4, 4,R_SS);
-	int i;
-	for ( i = 0; i < 4; i++)
-	{
-		ebp->args[i] = swaddr_read(addr+8+4*i, 4,R_SS);
-	}
-	
-}
-
-
-static int  cmd_bt(char * args){
-	int a = 0 ;
-	PSF current_ebp;
-	char tmp[32];
-
-	swaddr_t addr = reg_l(R_EBP);
-	current_ebp.ret_addr = cpu.eip;
-	while(addr>0){
-		printf("#%d	0x%08x in\t",a++,current_ebp.ret_addr);
-
-		get_func_from_addr(tmp,current_ebp.ret_addr);
-
-		printf("%s\t",tmp);
-		read_ebp(addr,&current_ebp);
-		if(strcmp(tmp,"main")==0)printf("( )\n");
-		else {
-			printf("( %d, %d, %d, %d )\n", current_ebp.args[0], current_ebp.args[1], current_ebp.args[2], current_ebp.args[3]);
-		}
-		addr = current_ebp.prev_ebp;
-	}
-	return 0;
-}
-
 
 static int cmd_help(char *args);
 
@@ -195,8 +203,8 @@ static struct {
         { "p", "Evaluate the value of expression", cmd_p },
 	{ "w", "Set watchpoint", cmd_w },
 	{ "d", "Delete watchpoint", cmd_d },
-	{"bt","print stack chain",cmd_bt}
-
+	{ "bt" , "Print stack frame",cmd_bt},
+	{ "page", "Convert virtual address to physical address", cmd_page},
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
